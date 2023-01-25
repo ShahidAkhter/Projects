0669565a5873a7c0f89ed185667f637ec97668d5
Shahid Akhter <shahidakhter5661@gmail.com>
2023-01-25 16:34:57 +0530

    Removing Code repetition

--- Binary/script.js
+++ Binary/script.js
@@ -1,7 +1,23 @@
 number = 0;
-arrNum = [1, 2, 4, 8, 16, 32, 64, 128]
-arrbuttonkeys=["a","s","d","f","g","h","j","k","l",";"]
-
+arrNum = [1 / 256, 1 / 128, 1 / 64, 1 / 32, 1 / 16, 1 / 8, 1 / 4, 1 / 2, 1, 2, 4, 8, 16, 32, 64, 128,]
+arrbuttonkeys = ["a", "s", "d", "f", "j", "k", "l", ";", "q", "w", "e", "r", "u", "i", "o", "p"]
+html = ""
+for (let i = 0; i < arrNum.length; i++) {
+    // const element = arrbuttonkeys[i];
+    html += `<div class="buttons" id="button${i + 1}">0</div>`;
+}
+document.getElementById('buttonContainer').innerHTML = html;
+html = '';
+for (let i = 0; i < arrbuttonkeys.length; i++) {
+    // const element = arrNum[i];
+    if (arrNum[i] >= 1) {
+        html += `<div class="buttons buttonDesc" id="buttonDesc${i}">.${arrbuttonkeys[i]}</div>`;
+    } else {
+        html += `<div class="buttons buttonDesc" id="buttonDesc${i}">${arrbuttonkeys[i]}</div>`;
+    }
+}
+document.getElementById('buttonContainerDesc').innerHTML = html;
+html = '';
 let ChangeText = (container, key, val) => {
     document.body.addEventListener("keydown", (event) => {
         if (event.key == key) {
@@ -15,10 +31,10 @@ let ChangeText = (container, key, val) => {
 let switchChanger = (container, val) => {
     if (container.innerText == '0') {
         container.innerText = "1";
-        container.style.color="green"
+        container.style.color = "green"
     } else {
         container.innerText = "0";
-        container.style.color="red"
+        container.style.color = "red"
     }
     Calculations(container, val);
     document.getElementById('textID').innerText = number
@@ -32,11 +48,8 @@ let Calculations = (container, val) => {
     }
     return number;
 }
-ChangeText(document.getElementById("button1"), arrbuttonkeys[0], arrNum[arrNum.length - 1]);
-ChangeText(document.getElementById("button2"), arrbuttonkeys[1], arrNum[arrNum.length - 2]);
-ChangeText(document.getElementById("button3"), arrbuttonkeys[2], arrNum[arrNum.length - 3]);
-ChangeText(document.getElementById("button4"), arrbuttonkeys[3], arrNum[arrNum.length - 4]);
-ChangeText(document.getElementById("button5"), arrbuttonkeys[6], arrNum[arrNum.length - 5]);
-ChangeText(document.getElementById("button6"), arrbuttonkeys[7], arrNum[arrNum.length - 6]);
-ChangeText(document.getElementById("button7"), arrbuttonkeys[8], arrNum[arrNum.length - 7]);
-ChangeText(document.getElementById("button8"), arrbuttonkeys[9], arrNum[arrNum.length - 8]); 
+
+for (let i = 0; i < arrNum.length; i++) {
+    // const element = arrNum[i];
+    ChangeText(document.getElementById(`button${i + 1}`), arrbuttonkeys[i], arrNum[arrNum.length - (i + 1)]);
+}
