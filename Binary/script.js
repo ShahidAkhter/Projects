3f74b6df0a0e64a479b8f90b42e7519224b72a9d
Shahid Akhter <shahidakhter5661@gmail.com>
2023-01-28 20:13:17 +0530

    Updating Content for Resposiveness

--- Binary/script.js
+++ Binary/script.js
@@ -2,22 +2,19 @@ number = 0;
 arrNum = [1 / 256, 1 / 128, 1 / 64, 1 / 32, 1 / 16, 1 / 8, 1 / 4, 1 / 2, 1, 2, 4, 8, 16, 32, 64, 128,]
 arrbuttonkeys = ["a", "s", "d", "f", "j", "k", "l", ";", "q", "w", "e", "r", "u", "i", "o", "p"]
 html = ""
-for (let i = 0; i < arrNum.length; i++) {
-    // const element = arrbuttonkeys[i];
-    html += `<div class="buttons" id="button${i + 1}">0</div>`;
-}
-document.getElementById('buttonContainer').innerHTML = html;
-html = '';
 for (let i = 0; i < arrbuttonkeys.length; i++) {
-    // const element = arrNum[i];
-    if (arrNum[i] >= 1) {
-        html += `<div class="buttons buttonDesc" id="buttonDesc${i}">.${arrbuttonkeys[i]}</div>`;
-    } else {
-        html += `<div class="buttons buttonDesc" id="buttonDesc${i}">${arrbuttonkeys[i]}</div>`;
-    }
+    // const element = arrbuttonkeys[i];
+    html += `<div class="buttonsDescContainer flex alignItemsCenter justifyContentCenter flex-col" id="buttonContains${i}">
+                <div class="buttonsDescContainer flex alignItemsCenter justifyContentCenter flex-col" id="buttonContainerDesc${i}">
+                    <div class="buttons buttonDesc" id="buttonDesc${i}">${arrbuttonkeys[i]}</div>
+                </div>
+                <div class="buttonsContainer flex alignItemsCenter justifyContentCenter flex-col" id="buttonContainer${i}">
+                    <div class="buttons" id="button${i}">0</div>
+                </div>
+            </div>`;
 }
-document.getElementById('buttonContainerDesc').innerHTML = html;
-html = '';
+document.getElementById('allButtons').innerHTML = html;
+html="";
 let ChangeText = (container, key, val) => {
     document.body.addEventListener("keydown", (event) => {
         if (event.key == key) {
@@ -51,5 +48,5 @@ let Calculations = (container, val) => {
 
 for (let i = 0; i < arrNum.length; i++) {
     // const element = arrNum[i];
-    ChangeText(document.getElementById(`button${i + 1}`), arrbuttonkeys[i], arrNum[arrNum.length - (i + 1)]);
+    ChangeText(document.getElementById(`button${i}`), arrbuttonkeys[i], arrNum[arrNum.length - (i + 1)]);
 }
