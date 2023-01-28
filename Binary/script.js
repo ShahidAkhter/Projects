6b7f4cc09a36c431f47b8bc438e50c48bdf2a5e2
Shahid Akhter <shahidakhter5661@gmail.com>
2023-01-28 21:10:11 +0530

    UI changes

--- Binary/script.js
+++ Binary/script.js
@@ -1,27 +1,32 @@
 number = 0;
 arrNum = [1 / 256, 1 / 128, 1 / 64, 1 / 32, 1 / 16, 1 / 8, 1 / 4, 1 / 2, 1, 2, 4, 8, 16, 32, 64, 128,]
 arrbuttonkeys = ["a", "s", "d", "f", "j", "k", "l", ";", "q", "w", "e", "r", "u", "i", "o", "p"]
-html = ""
-html0=""
-for (let i = 0; i < arrbuttonkeys.length; i++) {
-    // const element = arrbuttonkeys[i];
-    if (arrNum[i] >= 1) {
-        html0 = `<div class="buttons buttonDesc" id="buttonDesc${i}">.${arrbuttonkeys[i]}</div>`;
-    } else {
-        html0 = `<div class="buttons buttonDesc" id="buttonDesc${i}">${arrbuttonkeys[i]}</div>`;
+// n=prompt("Enter the Number: ")
+let myHTML=(n)=>{
+    html = ""
+    html0=""
+    for (let i = 0; i < n; i++) {
+        // const element = arrbuttonkeys[i];
+        if (arrNum[i] >= 1) {
+            html0 = `<div class="buttons buttonDesc" id="buttonDesc${i}">.${arrbuttonkeys[i]}</div>`;
+        } else {
+            html0 = `<div class="buttons buttonDesc" id="buttonDesc${i}">${arrbuttonkeys[i]}</div>`;
+        }
+        html += `<div class="buttonContainsTwoButtons flex alignItemsCenter justifyContentCenter flex-col" id="buttonContains${i}">
+        <div class="buttonsDescContainers flex alignItemsCenter justifyContentCenter flex-col" id="buttonContainerDesc${i}">
+        ${html0}
+        </div>
+        <div class="buttonsContainer flex alignItemsCenter justifyContentCenter flex-col" id="buttonContainer${i}">
+        <div class="buttons" id="button${i}">0</div>
+        </div>
+        </div>`;
     }
-    html += `<div class="buttonsDescContainer flex alignItemsCenter justifyContentCenter flex-col" id="buttonContains${i}">
-                <div class="buttonsDescContainer flex alignItemsCenter justifyContentCenter flex-col" id="buttonContainerDesc${i}">
-                    ${html0}
-                </div>
-                <div class="buttonsContainer flex alignItemsCenter justifyContentCenter flex-col" id="buttonContainer${i}">
-                    <div class="buttons" id="button${i}">0</div>
-                </div>
-            </div>`;
+    document.getElementById('allButtons').innerHTML = html;
+    html="";
+    html0="";
+    return true;
 }
-document.getElementById('allButtons').innerHTML = html;
-html="";
-html0="";
+myHTML(arrbuttonkeys.length);
 let ChangeText = (container, key, val) => {
     document.body.addEventListener("keydown", (event) => {
         if (event.key == key) {
